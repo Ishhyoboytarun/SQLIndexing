362645e28057ec52d5e30e6656e1a54905f804c4
Tarun Gusain <36428256+Ishhyoboytarun@users.noreply.github.com>
2023-04-12 08:36:18 +0530

    Create BestPractices.sql

---
+++ BestPractices.sql
@@ -0,0 +1,21 @@
+/*
+Creating and maintaining indexes is a critical aspect of database optimization. 
+While indexes can greatly improve query performance, they can also have a negative impact on 
+insert, update, and delete operations, as well as consume additional storage space. 
+Therefore, it's important to follow best practices when creating and maintaining indexes 
+to ensure optimal database performance.
+
+
+Here are some best practices for creating and maintaining indexes in SQL:
+
+  1. #Identify the most frequently accessed columns: Before creating an index, identify the columns that are frequently accessed in queries. Indexes should be created on columns that are frequently used in WHERE, JOIN, and ORDER BY clauses, as well as columns that are used in aggregate functions like SUM or COUNT.
+
+  2. #Create indexes on smaller tables: Indexes are most effective on smaller tables that have fewer rows. For larger tables, creating indexes on a subset of columns can be more effective.
+
+  3. #Use the right type of index: Different types of indexes, such as B-tree, hash, and bitmap, are optimized for different types of queries. Choose the right type of index for your specific use case.
+
+  4. #Avoid over-indexing: Creating too many indexes can have a negative impact on database performance, as it can slow down insert, update, and delete operations. Only create indexes that are necessary for query performance.
+
+  5. #Rebuild or reorganize indexes regularly: Over time, indexes can become fragmented and lose their efficiency. Rebuilding or reorganizing indexes can improve performance and reduce storage space.
+  
+ */
